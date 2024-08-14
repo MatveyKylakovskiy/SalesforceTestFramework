b5cc12dac9a6b1603a9ae8a23295ccd235dfafc5
matveykylak <matveykylak@gmail.com>
2024-08-15 01:16:19 +0300

    0116

--- SalesforceTestFramework/UI/UITests/UnitTest1.cs
+++ SalesforceTestFramework/UI/UITests/UnitTest1.cs
@@ -1,17 +1,19 @@
 using NUnit.Allure.Attributes;
-using Allure.NUnit;
+using NUnit.Allure;
+using NUnit.Allure.Core;
 
 namespace SalesforceTestFramework.UI.UITests
 {
     [AllureNUnit]
     public class Tests : BaseTestUI
     {
-        [AllureOwner("Matvey Kylakovskiy")]
-        [AllureLink("Website", "https://qatech5-dev-ed.develop.my.salesforce.com")]
+        
         [AllureIssue("UI-123")]
         [AllureTms("TMS-456")]
         [AllureDescription("Login Test Positive")]
         [Test]
+        [AllureOwner("Matvey Kylakovskiy")]
+        [AllureLink("Website", "https://qatech5-dev-ed.develop.my.salesforce.com")]
         public void Test1()
         {
             Assert.Pass();
