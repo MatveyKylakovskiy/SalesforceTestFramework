ba793c6e609c3df9337e5ce5e198d3082695b353
matveykylak <matveykylak@gmail.com>
2024-08-14 11:25:55 +0300

    Add project files.

---
+++ SalesforceTestFramework/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SalesforceTestFramework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
