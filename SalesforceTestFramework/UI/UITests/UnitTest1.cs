a386794ff8b072435c9034bbbca1dbe18abae776
matveykylak <matveykylak@gmail.com>
2024-08-14 17:50:59 +0300

    Allure report 2

--- SalesforceTestFramework/UI/UITests/UnitTest1.cs
+++ SalesforceTestFramework/UI/UITests/UnitTest1.cs
@@ -1,8 +1,11 @@
 
+using NUnit.Allure.Attributes;
+
 namespace SalesforceTestFramework.UI.UITests
 {
     public class Tests : BaseTestUI
     {
+        [AllureTag("NUnit", "Example")]
         [Test]
         public void Test1()
         {
